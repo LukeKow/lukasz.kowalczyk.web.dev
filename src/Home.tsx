13f8b6093b114c3b71b25c3ea99ed336db2e59e3
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2025-05-24 19:55:04 +0200

    initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material"
+
+function App() {
+
+	return (
+		<Box>
+			<Typography variant="h1">
+				Hej, jeśli tu jesteś to znaczy, że wpadłeś/wpadłaś!
+			</Typography>
+		</Box>
+	);
+}
+
+export default App
