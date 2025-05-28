c219d29b8be486478d1bf932dba93b2953a8c952
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2025-05-28 10:38:03 +0200

    set default font family

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,7 +3,7 @@ import { type ButtonProps, buttonColorMap } from "./ButtonTypes";
 export const Button = ({ color, onClick, label, variant }: ButtonProps) => {
 	return (
 		<button
-			className={`${buttonColorMap[color][variant]} hover:cursor-pointer shadow-md rounded-xs font-fira-sans
+			className={`${buttonColorMap[color][variant]} hover:cursor-pointer shadow-md rounded-xs
 			py-[5px] px-5`}
 			onClick={onClick}
 		>
