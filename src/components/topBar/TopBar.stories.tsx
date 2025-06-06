20fb8ba00c0377b57ee83203fff6e8d8c4c866d9
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2025-06-06 20:31:50 +0200

    bug fix

--- src/components/topBar/TopBar.stories.tsx
+++ src/components/topBar/TopBar.stories.tsx
@@ -1,37 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { TopBar } from "./TopBar";
-import type { TopBarProps } from "./TopBarTypes";
-import { useRef } from "react";
 
 const meta: Meta<typeof TopBar> = {
   component: TopBar,
-};
-
-const StoryWithRefs = (props: TopBarProps) => {
-  const nullRef = useRef(null);
-  const refs = [
-    {
-      ref: nullRef,
-      text: "dummy text 1",
-    },
-    {
-      ref: nullRef,
-      text: "dummy text 2",
-    },
-    {
-      ref: nullRef,
-      text: "dummy text 3",
-    },
-  ];
-  return <TopBar {...props} menuItems={refs} />;
 };
 
 export default meta;
 type Story = StoryObj<typeof TopBar>;
 
 export const Primary: Story = {
-  render: (args) => <StoryWithRefs {...args} />,
   args: {
     name: "Łukasz Kowalczyk",
   },
--- src/components/topBar/components/menu/Menu.stories.tsx
+++ src/components/topBar/components/menu/Menu.stories.tsx
@@ -1,11 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 
 import { Menu } from "./Menu";
 
 const meta: Meta<typeof Menu> = {
   component: Menu,
-  args: { onClick: fn() },
 };
 
 export default meta;
@@ -13,6 +11,6 @@ type Story = StoryObj<typeof Menu>;
 
 export const Primary: Story = {
   args: {
-    color: "#FFB738",
+    pointerColor: "blue",
   },
 };
