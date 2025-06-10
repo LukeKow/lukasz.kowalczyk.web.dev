725189afcb1b4b874858907490e8a40f04e0b983
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2025-06-10 15:25:08 +0200

    add header

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,3 +1,5 @@
+import { PointerWithText } from "../shared/pointerWithText/PointerWithText";
+import { Typography } from "../shared/typography/Typography";
 import { TopBar } from "../topBar/TopBar";
 import { type ProjectsProps } from "./ProjectsTypes";
 
@@ -5,7 +7,21 @@ export const Projects = ({ title }: ProjectsProps) => {
   return (
     <>
       <TopBar name="Łukasz Kowalczyk" />
-      <p>{title}</p>
+      <div className="px-5">
+        <PointerWithText
+          pointerColor="blue"
+          text="PROJECTS"
+          fontType="subheader"
+          textColor="blue"
+        />
+        <Typography
+          textColor="blue"
+          fontType="body"
+          text="Niektóre projekty, w których brałem udział"
+        />
+      </div>
+      {/* TODO: Create ProjectCard component */}
+      <p> Here's a place for project Card</p>
     </>
   );
 };
